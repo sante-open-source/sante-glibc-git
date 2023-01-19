5d8505705d8cee499b922f43adab3951980eafbf
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-01-19 20:27:38 +0800

    Build form with phase

--- build.sh
+++ build.sh
@@ -13,47 +13,68 @@ case $arch in
 esac
 toolchain_prefix=$HOME/dist/$target-sante-elf-gcc/$target-sante-elf-gcc-git
 export PATH="$PATH:$toolchain_prefix/bin"
-git clone --depth=1 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git &> /dev/null
+
+# Clone linux, binutils, gcc and glibc
+urls=(git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+      git://sourceware.org/git/binutils-gdb.git
+      git://gcc.gnu.org/git/gcc.git
+      git://sourceware.org/git/glibc.git)
+for url in "${urls[@]}"; do
+	git clone --depth=1 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git &> /dev
+/null
+done
+
+# Install linux headers
 cd linux
 make ARCH=$(echo $target | cut -d '-' -f 1) INSTALL_HDR_PATH=$toolchain_prefix headers_install
-cd .. && rm -rf linux
-git clone --depth=1 git://sourceware.org/git/binutils-gdb.git &> /dev/null
-mkdir binutils-gdb-build && cd binutils-gdb-build
+
+# Build binutils and gdb
+cd .. && mkdir binutils-gdb-build && cd binutils-gdb-build
 ../binutils-gdb/configure --target=$target \
 	                  --prefix=$toolchain_prefix \
-		          --disable-nls
+		          --disable-multilib
 make -j`nproc`
 make install
-cd .. && rm -rf binutils-gdb
-git clone --depth=1 git://gcc.gnu.org/git/gcc.git &> /dev/null
-cd gcc
+
+# Build gcc (stage 1)
+cd ../gcc
 ./contrib/download_prerequisites
 cd .. && mkdir build-gcc && cd build-gcc
 ../gcc/configure --target=$target \
 	         --prefix=$toolchain_prefix \
-		 --disable-nls \
-		 --disable-multilib \
-		 --without-headers \
-		 --with-newlib
-make -j`nproc` all-gcc all-target-libgcc
-make install-gcc install-target-libgcc
-cd ..
-git clone --depth=1 git://sourceware.org/git/glibc.git &> /dev/null
-mkdir build-glibc && cd build-glibc
+		 --enable-languages=c,c++ \
+		 --disable-multilib
+make -j`nproc` all-gcc
+make install-gcc
+
+# Build glibc (stage 1)
+cd .. && mkdir build-glibc && cd build-glibc
 ../glibc/configure --target=$target \
+	           --host=$target \
+		   --build=x86_64-linux-gnu \
 	           --prefix=$toolchain_prefix \
-		   --disable-nls \
+		   --disable-multilib
 		   --with-headers=$toolchain_prefix/include \
-		   --without-selinux
+		   --without-selinux \
+		   libc_cv_forced_unwind=yes
+make install-bootstrap-headers=yes install-headers
+make -j`nproc` csu/subdir_lib
+install csu/crt1.o csu/crti.o csu/crtn.o $toolchain_prefix/lib
+$target-gcc -nostdlib -nostartfiles -shared -xc /dev/null -o $toolchain_prefix/lib/libc.so
+touch $toolchain_prefix/include/gnu/stubs.h
+
+# Build gcc (stage 2)
+cd ../build-gcc
+make -j`nproc` all-target-libgcc
+make install-target-libgcc
+
+# Build glibc (stage 2)
+cd ../build-glibc
 make -j`nproc`
 make install
+
+# Build libstdc++
 cd ../build-gcc
-rm -rf *
-../gcc/configure --target=$target \
-	         --prefix=$toolchain_prefix \
-		 --enable-languages=c,c++ \
-		 --disable-nls \
-		 --disable-multilib
 make -j`nproc`
 make install
 ls $toolchain_prefix/bin
